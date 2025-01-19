426c6c8ce821f77eb428f9e9710854cd4db33ec1
luca@schluer.eu <luca@schluer.eu>
2025-01-20 00:01:35 +0100

    changed telescope keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,11 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',    
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
     config = function()
         require("telescope").setup({})
 
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>ps", function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
+        vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
     end
 }
