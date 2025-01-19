5d455a2da8a25bb1b7f9d241d10813d0cb799901
luca@schluer.eu <luca@schluer.eu>
2025-01-19 23:38:31 +0100

    Added for active buffers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,6 +15,20 @@ return {
     },
 
     config = function()
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = {buffer = event.buf}
+
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+            end,
+        })
+
         require("conform").setup({
             formatters_by_ft = {
             }
@@ -92,8 +106,8 @@ return {
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' }, -- For luasnip users.
             }, {
-                { name = 'buffer' },
-            })
+                    { name = 'buffer' },
+                })
         })
 
         vim.diagnostic.config({
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,13 @@
-local builtin = require("telescope.builtin")
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',    
     config = function()
-	require("telescope").setup({})
+        require("telescope").setup({})
 
-	local builtin = require("telescope.builtin")
-	vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-	vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-	vim.keymap.set("n", "<leader>ps", function()
-		builtin.grep_string({ search = vim.fn.input("Grep > ") })
-	end)
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+        vim.keymap.set("n", "<leader>ps", function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
     end
 }
